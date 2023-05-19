ae3b21842e3b2b33ce3450bea5c41a4597dfbadb
Rishikesh <91647089+i-rishikesh@users.noreply.github.com>
2023-05-19 08:37:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,104 @@
+for(var i=0;i<document.querySelectorAll(".drum").length;i++){
+    document.querySelectorAll("button")[i].addEventListener("click",function()
+{
+    var buttonInnerHTML=this.innerHTML;
+    switch(this.innerHTML){
+        case "w":
+            var audio=new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio=new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio=new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio=new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio=new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio=new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio=new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        default:
+            console.log(buttonInnerHTML);
+    }
+    buttonAnimation(buttonInnerHTML);
+})};
+document.addEventListener("keypress",function(event){
+    var buttonInnerHTML=event.key;
+
+    
+    switch(event.key){
+        case "w":
+            var audio=new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio=new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio=new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio=new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio=new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio=new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio=new Audio("sounds/kick-bass.mp3");
+            audio.play(); 
+            break;
+        default:
+            console.log(buttonInnerHTML);
+    }
+    buttonAnimation(event.key);
+
+});
+function buttonAnimation(currentKey){
+    var activeButton=document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var audio=new Audio("sounds/tom-1.mp3");
+// audio.play();
